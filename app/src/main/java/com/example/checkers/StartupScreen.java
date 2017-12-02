47c40d01ac43a5f26b3f4743bd0c7170b558ea94
ChrisGuerrette <chrisg2706@gmail.com>
2017-12-02 12:42:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/checkers/StartupScreen.java
@@ -0,0 +1,13 @@
+package com.example.checkers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartupScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup_screen);
+    }
+}
