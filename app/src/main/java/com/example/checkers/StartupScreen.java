fb21afbbcaf213e0f11d1a408486c910b8a48e22
ChrisGuerrette <chrisg2706@gmail.com>
2017-12-03 17:43:02 -0500

    Added singleplayer select screen and functionality to get to it

--- app/src/main/java/com/example/checkers/StartupScreen.java
+++ app/src/main/java/com/example/checkers/StartupScreen.java
@@ -1,7 +1,9 @@
 package com.example.checkers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartupScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class StartupScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup_screen);
+    }
+
+    public void goToSingleplayerSelectScreen(View view){
+        Intent intent = new Intent(this, SingleplayerSelectScreen.class);
+        startActivity(intent);
     }
 }
